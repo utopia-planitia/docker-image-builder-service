04fadc168965f8d9b87e39636cae655b94c4e14c
David Sauer <davedamoon@gmail.com>
2018-02-22 17:57:36 +0100

    wait for files to appear in s3 after upload

--- caching-scripts/save.sh
+++ caching-scripts/save.sh
@@ -13,3 +13,17 @@ DATE=$(date +%s)
 HASH=$(docker history -q $1 | md5sum | head -c 32)
 echo -e "$DATE\n$HASH" | mc pipe cache/$CACHE_BUCKET/$2.meta
 docker save $1 $(docker history -q $1 | grep -v missing) | mc pipe cache/$CACHE_BUCKET/$2
+
+set +e
+echo -n waiting for meta data
+until mc stat cache/$CACHE_BUCKET/$2.meta; do
+    sleep 1
+    echo -n "."
+done
+echo
+echo -n waiting for data file
+until mc stat cache/$CACHE_BUCKET/$2 | grep -v meta | grep "Name      :"; do
+    sleep 1
+    echo -n "."
+done
+echo
