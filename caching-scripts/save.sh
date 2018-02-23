df056153a8bd142141724a13b16132ed6bf72377
David Sauer <davedamoon@gmail.com>
2018-02-23 17:00:47 +0100

    do not (re)save unchanged build cache layers

--- caching-scripts/save.sh
+++ caching-scripts/save.sh
@@ -8,6 +8,14 @@ if [ $? != 0 ]; then
   mc mb cache/$CACHE_BUCKET
 fi
 
+
+S3_HASH=$(mc cat cache/$CACHE_BUCKET/$2.meta | tail -n 1)
+LOCAL_HASH=$(docker history -q $1 | md5sum | head -c 32)
+if [ "${S3_HASH}" == "${LOCAL_HASH}" ]; then
+  echo "hashes match, skip build cache save"
+  exit 11
+fi
+
 set -e
 DATE=$(date +%s)
 HASH=$(docker history -q $1 | md5sum | head -c 32)
