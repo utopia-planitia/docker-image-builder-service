cef15c3c9cd2aece301a1545595a5fd7e6e2f0cb
David Sauer <davedamoon@gmail.com>
2018-02-23 16:50:39 +0100

    wait for s3 to be available before builder startup

--- caching-scripts/entrypoint.sh
+++ caching-scripts/entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+TIMEOUT="timeout"
+$TIMEOUT 1 true
+if [ $? != 0 ]; then
+  TIMEOUT="timeout -t"
+fi
+
+$TIMEOUT 15 bash -c "until echo > /dev/tcp/$CACHE_ENDPOINT_SERVER/$CACHE_ENDPOINT_PORT; do sleep 0.5; done"
+if [ $? != 0 ]; then
+  echo s3 endpoint is not available
+  exit 1
+fi
+
 mc config host ls | grep $CACHE_SECRET_KEY > /dev/null
 if [ $? != 0 ]; then
-  mc config host add cache $CACHE_ENDPOINT $CACHE_ACCESS_KEY $CACHE_SECRET_KEY
+  mc config host add cache http://$CACHE_ENDPOINT_SERVER:$CACHE_ENDPOINT_PORT/ $CACHE_ACCESS_KEY $CACHE_SECRET_KEY
 fi
 
 exec "$@"
--- etc/minikube.mk
+++ etc/minikube.mk
@@ -20,7 +20,8 @@ minikube-cli:
 	docker run -ti --rm \
 		--dns 10.96.0.10 --dns-search container-image-builder.svc.cluster.local \
 		-e DOCKER_HOST=tcp://docker:2375 \
-		-e CACHE_ENDPOINT=http://minio:9000/ \
+		-e CACHE_ENDPOINT_SERVER=minio \
+		-e CACHE_ENDPOINT_PORT=9000 \
 		-e CACHE_BUCKET=image-layers \
 		-e CACHE_ACCESS_KEY=8Q9U4RBHKKB6HU70SRZ1 \
 		-e CACHE_SECRET_KEY=oxxT2iqBlW6lgaDVe8ll6mP8z/OSVIUnn9cB4+Q0 \
@@ -33,7 +34,8 @@ minikube-tests:
 	docker run -ti --rm \
 		--dns 10.96.0.10 --dns-search container-image-builder.svc.cluster.local \
 		-e DOCKER_HOST=tcp://docker:2375 \
-		-e CACHE_ENDPOINT=http://minio:9000/ \
+		-e CACHE_ENDPOINT_SERVER=minio \
+		-e CACHE_ENDPOINT_PORT=9000 \
 		-e CACHE_BUCKET=image-layers \
 		-e CACHE_ACCESS_KEY=8Q9U4RBHKKB6HU70SRZ1 \
 		-e CACHE_SECRET_KEY=oxxT2iqBlW6lgaDVe8ll6mP8z/OSVIUnn9cB4+Q0 \
