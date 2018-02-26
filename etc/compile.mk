087e2062d136fa6a02621cff7d8b8f60c11f284f
David Sauer <davedamoon@gmail.com>
2018-02-26 04:44:08 +0100

    use circleci / move to github

--- etc/compile.mk
+++ etc/compile.mk
@@ -1,8 +1,14 @@
 
 builder: $(shell find ./cmd/builder -type f)
 #	CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o builder ./cmd/builder
+	go get -t ./cmd/builder/...
 	go build -o builder ./cmd/builder
 
 dispatcher: $(shell find ./cmd/dispatcher -type f)
 #	CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o dispatcher ./cmd/dispatcher
+	go get -t ./cmd/builder/...
 	go build -o dispatcher ./cmd/dispatcher
+
+clean:
+	unlink builder
+	unlink dispatcher
