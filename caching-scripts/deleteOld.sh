3a7c5ffafce8ff1efc9afb09485c3ba966952336
David Sauer <davedamoon@gmail.com>
2018-03-04 14:54:16 +0100

    used shellcheck to improve shell scripts

--- caching-scripts/deleteOld.sh
+++ caching-scripts/deleteOld.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 #http://shout.setfive.com/2011/12/05/deleting-files-older-than-specified-time-with-s3cmd-and-bash/
 
-olderThan=$((`date +%s` - $CACHE_MAX_AGE))
+CURRENT_DATE=$(date +%s)
+olderThan=$(("$CURRENT_DATE - $CACHE_MAX_AGE"))
 
-mc ls cache/$CACHE_BUCKET | while read -r line;
+mc ls "cache/$CACHE_BUCKET" | while read -r line;
 do
-  createDate=`echo $line|awk {'print $1" "$2'}`
-  createDate=`date -d"${createDate:1}" +%s`
+  createDate=$(echo "$line" | awk "{'print $1" "$2'}")
+  createDate=$(date -d"${createDate:1}" +%s)
   if [[ $createDate -lt $olderThan ]]; then 
       echo "deleteing $line"
-      fileName=`echo $line|awk {'print $5'}`
+      fileName=$(echo "$line" | awk "{'print $5'}")
       if [[ $fileName != "" ]]; then
           mc rm "cache/$CACHE_BUCKET/$fileName"
       fi
   fi
 done;
 
-mc ls -I cache/$CACHE_BUCKET | while read -r line;
+mc ls -I "cache/$CACHE_BUCKET" | while read -r line;
 do
-  createDate=`echo $line|awk {'print $1" "$2'}`
-  createDate=`date -d"${createDate:1}" +%s`
+  createDate=$(echo "$line" | awk "{'print $1" "$2'}")
+  createDate=$(date -d"${createDate:1}" +%s)
   if [[ $createDate -lt $olderThan ]]; then 
       echo "deleteing $line"
-      fileName=`echo $line|awk {'print $5'}`
+      fileName=$(echo "$line" | awk "{'print $5'}")
       if [[ $fileName != "" ]]; then
           mc rm "cache/$CACHE_BUCKET/$fileName"
       fi
--- caching-scripts/load.sh
+++ caching-scripts/load.sh
@@ -5,15 +5,15 @@
 
 set -e
 
-S3_HASH=$(mc cat cache/$CACHE_BUCKET/$2.meta | tail -n 1)
+S3_HASH=$(mc cat "cache/$CACHE_BUCKET/$2.meta" | tail -n 1)
 if [ "${S3_HASH}" == "" ]; then
   echo "meta file in s3 is missing"
   exit 12
 fi
-LOCAL_HASH=$(docker history -q $1 | md5sum | head -c 32)
-if [ "${S3_HASH}" == "${LOCAL_HASH}" ]; then
+LOCAL_HASH=$(docker history -q "$1" | md5sum | head -c 32)
+if [ "$S3_HASH" == "$LOCAL_HASH" ]; then
   echo "hashes match, skip build cache load"
   exit 11
 fi
 
-mc cat cache/$CACHE_BUCKET/$2 | docker load
+mc cat "cache/$CACHE_BUCKET/$2" | docker load
