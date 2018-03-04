462a6659ceba524001d7ecef4f75d2369ea96d4a
David Sauer <davedamoon@gmail.com>
2018-03-04 11:33:31 +0100

    cache kubectl, minikube, minikube-state

--- etc/start-minikube.sh
+++ etc/start-minikube.sh
@@ -2,7 +2,6 @@
 
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
-export MINIKUBE_HOME=$HOME
 export CHANGE_MINIKUBE_NONE_USER=true
 mkdir -p $HOME/.kube
 touch $HOME/.kube/config
