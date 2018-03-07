ca1f42bc2466f1428a9a93f73a260b1ee93b3df1
David Sauer <davedamoon@gmail.com>
2018-03-07 18:18:34 +0100

    added 'go test' to circle ci

--- etc/minikube.mk
+++ etc/minikube.mk
@@ -36,10 +36,15 @@ cli: ##@development Opens a command line interface with development tools.
 		-v $(PWD):/project -w /project \
 		utopiaplanitia/docker-image-builder-devtools:latest sh
 
-.PHONY: tests
-tests: ##@development Runs the tests.
-	docker build -f docker/devtools/Dockerfile  -t utopiaplanitia/docker-image-builder-devtools:latest .
-	docker run -ti --rm \
+.PHONY: test
+test: ##@testing Runs all tests.
+	@$(MAKE) go-test
+	@$(MAKE) e2e-test
+
+.PHONY: e2e-test
+e2e-test: ##@testing Runs end to end tests.
+	@docker build -q -f docker/devtools/Dockerfile -t utopiaplanitia/docker-image-builder-devtools:latest .
+	@docker run -ti --rm \
 		--dns 10.96.0.10 --dns-search container-image-builder.svc.cluster.local \
 		-e DOCKER_HOST=tcp://docker:2375 \
 		-e CACHE_ENDPOINT_SERVER=minio \
@@ -49,6 +54,10 @@ tests: ##@development Runs the tests.
 		-e CACHE_SECRET_KEY=oxxT2iqBlW6lgaDVe8ll6mP8z/OSVIUnn9cB4+Q0 \
 		-v $(PWD):/project -w /project \
 		utopiaplanitia/docker-image-builder-devtools:latest bats tests
+
+.PHONY: go-test
+go-test: ##@testing Runs go (unit) tests.
+	go test -race ./...
 
 .PHONY: deploy
 deploy: ##@development Deploys the current code.
