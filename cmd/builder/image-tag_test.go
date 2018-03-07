3b636fe4464a4d83b5c090a065a303c9a51b1ec4
David Sauer <davedamoon@gmail.com>
2018-03-07 18:10:06 +0100

    fixed broken test

--- cmd/builder/image-tag_test.go
+++ cmd/builder/image-tag_test.go
@@ -8,33 +8,28 @@ import (
 var tagTests = []struct {
 	in  string
 	out *tag
-	err bool
 }{
-	{"cassandra", &tag{image: "cassandra", version: "latest"}, false},
-	{"cassandra:latest", &tag{image: "cassandra", version: "latest"}, false},
-	{"cassandra:2", &tag{image: "cassandra", version: "2"}, false},
+	{"cassandra", &tag{image: "cassandra", version: "latest"}},
+	{"cassandra:latest", &tag{image: "cassandra", version: "latest"}},
+	{"cassandra:2", &tag{image: "cassandra", version: "2"}},
 
-	{"user/image:2", &tag{image: "user/image", version: "2"}, false},
+	{"user/image:2", &tag{image: "user/image", version: "2"}},
 
-	{"registry.domain.tld/cassandra:2", &tag{image: "registry.domain.tld/cassandra", version: "2"}, false},
-	{"registry.domain.tld/user/image:2", &tag{image: "registry.domain.tld/user/image", version: "2"}, false},
+	{"registry.domain.tld/cassandra:2", &tag{image: "registry.domain.tld/cassandra", version: "2"}},
+	{"registry.domain.tld/user/image:2", &tag{image: "registry.domain.tld/user/image", version: "2"}},
 
-	{"registry.domain.tld:5000/image:3", &tag{image: "registry.domain.tld:5000/image", version: "3"}, false},
-	{"registry.domain.tld:5000/image:latest", &tag{image: "registry.domain.tld:5000/image", version: "latest"}, false},
-	{"registry.domain.tld:5000/image", &tag{image: "registry.domain.tld:5000/image", version: "latest"}, false},
+	{"registry.domain.tld:5000/image:3", &tag{image: "registry.domain.tld:5000/image", version: "3"}},
+	{"registry.domain.tld:5000/image:latest", &tag{image: "registry.domain.tld:5000/image", version: "latest"}},
+	{"registry.domain.tld:5000/image", &tag{image: "registry.domain.tld:5000/image", version: "latest"}},
 
-	{"registry.domain.tld:5000/user/image:3", &tag{image: "registry.domain.tld:5000/user/image", version: "3"}, false},
-	{"registry.domain.tld:5000/user/image:latest", &tag{image: "registry.domain.tld:5000/user/image", version: "latest"}, false},
-	{"registry.domain.tld:5000/user/image", &tag{image: "registry.domain.tld:5000/user/image", version: "latest"}, false},
+	{"registry.domain.tld:5000/user/image:3", &tag{image: "registry.domain.tld:5000/user/image", version: "3"}},
+	{"registry.domain.tld:5000/user/image:latest", &tag{image: "registry.domain.tld:5000/user/image", version: "latest"}},
+	{"registry.domain.tld:5000/user/image", &tag{image: "registry.domain.tld:5000/user/image", version: "latest"}},
 }
 
 func TestNewTag(t *testing.T) {
 	for _, tt := range tagTests {
-		tag, err := newTag(tt.in)
-		if (err != nil) != tt.err {
-			t.Errorf("newTag(%q) returned an error: %s", tt.in, err)
-			continue
-		}
+		tag := newTag(tt.in)
 		if !compare(tag, tt.out) {
 			t.Errorf("newTag(%q) => %q, want %q", tt.in, debugPrint(tag), debugPrint(tt.out))
 		}
