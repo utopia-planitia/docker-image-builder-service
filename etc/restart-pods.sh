87043003536c2c952b831dbec7f4a9848d626b9e
David Sauer <david.sauer@turbinekreuzberg.com>
2018-03-09 14:46:03 +0100

    added readinessProbe

--- etc/restart-pods.sh
+++ etc/restart-pods.sh
@@ -24,62 +24,14 @@ for _ in {1..150}; do # timeout for 5 minutes
   sleep 2
 done
 
-echo -n waiting for builder-0
-for _ in {1..150}; do # timeout for 5 minutes
-  if kubectl -n container-image-builder get po builder-0 | grep Running | grep 2/2 > /dev/null 2>&1; then
-      echo " done"
-      break
-  fi
-  echo -n .
-  sleep 2
-done
-
-echo -n waiting for builder-1
-for _ in {1..150}; do # timeout for 5 minutes
-  if kubectl -n container-image-builder get po builder-1 | grep Running | grep 2/2 > /dev/null 2>&1; then
-      echo " done"
-      break
-  fi
-  echo -n .
-  sleep 2
-done
-
-echo -n waiting for dns to point to minio
-for _ in {1..150}; do # timeout for 5 minutes
-  if dig minio.container-image-builder.svc.cluster.local @10.96.0.10 | grep "ANSWER SECTION" > /dev/null 2>&1; then
-      echo " done"
-      break
-  fi
-  echo -n .
-  sleep 2
-done
-
-echo -n waiting for dns to point to docker
-for _ in {1..150}; do # timeout for 5 minutes
-  if dig docker.container-image-builder.svc.cluster.local @10.96.0.10 | grep "ANSWER SECTION" > /dev/null 2>&1; then
-      echo " done"
-      break
-  fi
-  echo -n .
-  sleep 2
-done
-
-echo -n waiting for dns to point to builder-0
-for _ in {1..150}; do # timeout for 5 minutes
-  if dig builder-0.builder.container-image-builder.svc.cluster.local @10.96.0.10 | grep "ANSWER SECTION" > /dev/null 2>&1; then
-      echo " done"
-      break
-  fi
-  echo -n .
-  sleep 2
-done
-
-echo -n waiting for dns to point to builder-1
-for _ in {1..150}; do # timeout for 5 minutes
-  if dig builder-1.builder.container-image-builder.svc.cluster.local @10.96.0.10 | grep "ANSWER SECTION" > /dev/null 2>&1; then
-      echo " done"
-      break
-  fi
-  echo -n .
-  sleep 2
+for i in {0..1}; do
+  echo -n "waiting for builder-$i"
+  for _ in {1..150}; do # timeout for 5 minutes
+    if kubectl -n container-image-builder get po "builder-$i" | grep Running | grep 2/2 > /dev/null 2>&1; then
+        echo " done"
+        break
+    fi
+    echo -n .
+    sleep 2
+  done
 done
