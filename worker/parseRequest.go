c36171fb565955a0698522c0c01595d57f71f350
David Sauer <david.sauer@turbinekreuzberg.com>
2018-03-19 17:30:16 +0100

    only cache from current branch (higher priority) and latest (master branch) tag

--- worker/parseRequest.go
+++ worker/parseRequest.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func parseTagsAndBranches(r *http.Request) ([]*tag, []string, string, error) {
+func parseTagsAndBranches(r *http.Request) ([]*tag, string, error) {
 
 	tags := []*tag{}
 	for _, t := range r.URL.Query()["t"] {
@@ -17,19 +17,18 @@ func parseTagsAndBranches(r *http.Request) ([]*tag, []string, string, error) {
 
 	cfjson := r.URL.Query()["cachefrom"]
 	if len(cfjson) == 0 {
-		return tags, nil, "", nil
+		return tags, "", nil
 	}
 	if len(cfjson) > 1 {
-		return nil, nil, "", errors.New("cachefrom parameter is set multiple times")
+		return nil, "", errors.New("cachefrom parameter is set multiple times")
 	}
 	cf, err := decodeCachefromJSON(cfjson[0])
 	if err != nil {
-		return nil, nil, "", fmt.Errorf("failed to decode cache from json: %s", err)
+		return nil, "", fmt.Errorf("failed to decode cachefrom json: %s", err)
 	}
-	cacheFromBranches := filterCacheFromBranches(cf)
 	currentBranch := filterCurrentBranch(cf)
 
-	return tags, cacheFromBranches, currentBranch, nil
+	return tags, currentBranch, nil
 }
 
 func decodeCachefromJSON(cf string) ([]string, error) {
@@ -41,16 +40,6 @@ func decodeCachefromJSON(cf string) ([]string, error) {
 		return nil, fmt.Errorf("failed to json decode cachefrom: %s", err)
 	}
 	return l, nil
-}
-
-func filterCacheFromBranches(cf []string) []string {
-	var branches []string
-	for _, e := range cf {
-		if strings.HasPrefix(e, "branch=") {
-			branches = append(branches, e[len("branch="):])
-		}
-	}
-	return branches
 }
 
 func filterCurrentBranch(cf []string) string {
